e859c1b68e82c20021a7651c8755a6512562cb82
K-KawanSousa <108997278+K-KawanSousa@users.noreply.github.com>
2022-12-19 14:43:51 -0300

    my version of clock
    my version with more focused on neumorphism using box-shadow and drop-shadow

--- script/setClock.js
+++ script/setClock.js
@@ -1,18 +1,24 @@
-const 
-    deg = 6,
-    hrArrowCont = document.querySelector('.hour'),
-    minArrowCont = document.querySelector('.min'),
-    secArrowCont = document.querySelector('.sec'),
-    secondAudio = new Audio('/audio/second-audio.mp3')
+const
+    hrPointer = document.querySelector('#hour'),
+    minPointer = document.querySelector('#min'),
+    secPointer = document.querySelector('#sec'),
+    multiplier = 6, // value resulting from dividing 360 (total number of degrees) by 60 (maximum minutes and seconds)
+    secondSound = new Audio('/audio/second-audio.mp3')
 
+    updateHr()
     setInterval(()=>{
-    let date = new Date(),
-        hr = date.getHours() * 30,
-        mn = date.getMinutes() * deg,
-        sc = date.getSeconds() * deg;
-
-    hrArrowCont.style.transform = `rotate(${hr+(mn/12)}deg)`;
-    minArrowCont.style.transform = `rotate(${mn}deg)`
-    secArrowCont.style.transform = `rotate(${sc}deg)`;
-    secondAudio.play()
+        updateHr()
+        secondSound.play();
     }, 1000)
+
+function updateHr(){
+    let 
+        date = new Date(),
+        hr = date.getHours() * 30,// value resulting from dividing 360 (total number of degrees) by 30 (maximum hour in a day)
+        mn = date.getMinutes() * multiplier,
+        sc = date.getSeconds() * multiplier;
+
+    hrPointer.style.transform = `rotateZ(${hr + (mn / 12)}deg)`;
+    minPointer.style.transform = `rotateZ(${mn}deg)`;
+    secPointer.style.transform = `rotateZ(${sc}deg)`;
+}
